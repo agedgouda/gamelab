5080079f294b277722f1af01cb05529fcabb892d
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2024-11-01 10:33:01 -0700

    reorganize interface

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight font-data70 tracking-widest">
-            {{ __('Events') }}
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Appropriate Page Title') }}
         </h2>
     </x-slot>
 
